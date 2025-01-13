6546bc5d0c2c4ee9b9419902a0d875610f7561b0
USER <kevinpb.py@gmail.com>
2025-01-13 23:36:08 +0100

    da igual

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ other_courses_avarage = 4
 
 dalto_course = 1.5
 
+# Raw duration
+
+
+
 # Difference in duration
 
-difference_with_min = 1.5
-dalto_course = 1.5
+difference_with_min = 100 - dalto_course / other_courses_min * 100
+
+print(f"Dalto's course takes {difference_with_min}% less than the fastest course")
+
+difference_with_max = 100 - dalto_course * 1000 // other_courses_max / 10
+
+print(f"Dalto's course takes {difference_with_max}% less than the slowest course")
+
+difference_with_avarage = 100 - dalto_course / other_courses_avarage * 100
+
+print(f"Dalto's course takes {difference_with_avarage}% less than the avarage")
