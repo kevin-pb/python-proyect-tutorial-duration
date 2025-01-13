a0e90ce6026627d55a780f95ca007643472637d5
USER <kevinpb.py@gmail.com>
2025-01-13 22:56:05 +0100

    main add

---
+++ main.py
@@ -0,0 +1,14 @@
+# Duration avarage
+
+other_courses_min = 2.5
+
+other_courses_max = 7
+
+other_courses_avarage = 4
+
+dalto_course = 1.5
+
+# Difference in duration
+
+difference_with_min = 1.5
+dalto_course = 1.5
